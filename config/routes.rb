819a5ad8147491ee9c78d33740334eab75d699a5
jun-marron <j.kimura42016891@gmail.com>
2018-12-09 00:53:25 +0900

    root設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
