ceeda36606eab250375b131ac6844c287cbc0d40
jun-marron <j.kimura42016891@gmail.com>
2018-12-10 00:12:01 +0900

    registarations contoroller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for  :users
-  resources :users, only: [:show]
+  devise_for :users, :controllers => {
+     :registrations => "registrations"
+   }
+   resources :users, only: [:show]
 
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
