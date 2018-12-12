f583107105dbbb891a0fd9cffa1bf4bcb5a4ee49
jun-marron <j.kimura42016891@gmail.com>
2018-12-13 01:06:40 +0900

    ランダムid生成　field作成前

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_create :generate_code
+
   def after_sign_up_path_for(resource)
     user_path(resource)
   end
@@ -6,5 +8,9 @@ class RegistrationsController < Devise::RegistrationsController
 
       def sign_up_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+
+      def generate_code
+        self.code = SecureRandom.alphanumeric(7)
       end
 end
